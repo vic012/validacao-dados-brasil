6d8cc7c38e6fc12e37b5093e59ec048ebf374d2f
Pedro Henrique <henriquevic012@gmail.com>
2021-06-21 17:31:36 -0300

    Formatação de datas brasileiras

---
+++ datasBr.py
@@ -0,0 +1,45 @@
+from datetime import datetime, timedelta
+
+class DatasBr:
+
+	def __init__(self):
+		self.momento_cadastro = datetime.today()
+
+	@property
+	def data_formatada(self):
+		return self.momento_cadastro.strftime("O cadastro foi realizado em %d/%m/%Y às %H horas e %M minutos.")
+	
+	def mes_cadastro(self):
+		meses_do_ano = [
+			"Janeiro",
+			"Fevereiro",
+			"Março",
+			"Abril",
+			"Maio",
+			"Junho",
+			"Julho",
+			"Agosto",
+			"Setembro",
+			"Outubro",
+			"Novembro",
+			"Dezembro"
+		]
+		#Retorna um inteiro que sinaliza o mês do ano
+		mes_cadastro = self.momento_cadastro.month
+		return meses_do_ano[mes_cadastro -1]
+
+	def dia_semana(self):
+		dias_da_semana = [
+			"Domingo",
+			"Segunda-Feira",
+			"Terça-Feira",
+			"Quarta-Feira",
+			"Quinta-Feira",
+			"Sexta-Feira"
+		]
+		#Retorna o dia da semana
+		dia_semana = self.momento_cadastro.weekday()
+		return dias_da_semana[dia_semana + 1]
+
+	def tempo_cadastro(self):
+		
--- main.py
+++ main.py
@@ -1,38 +1,4 @@
-from telefones import Telefones
-import re
+from datasBr import DatasBr
 
-#Produra por um padrão número, letra, número (Exemplo 1a2)
-#padrao = "[0-9][a-z][0-9]"
-#texto = "123 1a2 1cc aa1"
-#Procura por um padrão número, letra, letra, número (Exemplo 1ab2)
-#padrao = "[0-9][a-z]{2}[0-9]"
-#texto = "123 1ab2 1cc aa1"
-#resposta = re.search(padrao, texto)
-#print(resposta.group())
-
-#Padrão de e-mail
-#\W 'Qualquer número de 0 até 9, letra de a até z ou 0 _(Underline)'
-'''padrao = "\w{5,50}@\w{3,10}.\w{2,3}.\w{2,3}"
-email = 'Vixeeeeeeeeeeeeeeeeeeeeeeeeeeeee chico012@gmail.com.br'
-resposta = re.search(padrao, email)
-#print(resposta.group())
-
-#Padrões para telefone
-padraoMolde = "(xx)aaaa-wwww"
-#Pode conter 4 ou 5 dígitos {4,5}
-telefone = "[0-9]{2}[0-9]{4,5}[0-9]{4}"
-texto = "Eu gosto do numero 2126451234 e outro 12314563135"
-#Encontra todas as correspondecias de um padrão em um texto
-respostaTel = re.findall(telefone, texto)
-print(respostaTel)'''
-
-telefone = "552126481234"
-
-telefone_obj = Telefones(telefone)
-print(telefone_obj)
-#Uso do () na rbiblioteca re (Captura e agrupa)
-#(pais)(area ou regiao)(5 primeiros dígitos do número)(4 últimos dígitos)
-#O ? indica que a parte indicada não é obrigatório
-#padrao = "([0-9]{2,3})([0-9]{2})([0-9]{4,5})([0-9]{4})"
-#resposta = re.findall(padrao, telefone)
-#print(resposta)
+cadastro = DatasBr()
+print(cadastro.data_formatada)
